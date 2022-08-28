0bf71cef423fb1f0b6142d789c23dccb29c229c9
seenkit <nukalekseenko@gmail.com>
2022-08-28 15:37:11 +0300

    Initial commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,7 +6,7 @@ public class Util {
     public static Connection getConnection() {
         Connection connection = null;
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/users", "root", "K60380833e479E");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/new_schema", "root", "12031992");
         } catch (SQLException e) {
             e.printStackTrace();
         }
